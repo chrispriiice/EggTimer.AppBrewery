c5320e972dcda4ecbede0e0d1738516a8c75a5b9
Chris Price <christopher.r.price@gmail.com>
2020-05-06 01:41:22 -0400

    Completed except for alarm sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var totalTime = 0
+    var secondsPassed = 0
+    var secondsRemaining = 0
+    var timer = Timer ()
+    var hardness = ""
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        timer.invalidate()
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        
+        hardness = sender.currentTitle!
+        titleLabel.text = hardness
+        totalTime = eggTimes[hardness]!
 
-
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self , selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
 }
